cc8cb4652cd42034ba52938c5acaa7de8d979dc2
Ho2bes <8696@holbertonstudents.com>
2024-10-04 15:44:17 +0200

    back to previous config, update dependance and babel.config and succeed to connect again front with emulator with fidzen_front in the main repo.

--- FidZen_front/babel.config.js
+++ FidZen_front/babel.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['@babel/plugin-transform-typescript', { allowNamespaces: true }],
     '@babel/plugin-syntax-jsx',
     '@babel/plugin-transform-react-jsx',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-private-methods',
+    '@babel/plugin-transform-flow-strip-types', // Ce plugin est crucial pour supporter Flow
   ],
 };
